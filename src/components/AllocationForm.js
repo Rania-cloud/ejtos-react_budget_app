05892aedb600c407578adcaf8cf5e1af2871f0ed
Rania-cloud <raniaas032006@outlook.fr>
2025-05-03 00:40:41 +0200

    Update AllocationForm.js

--- src/components/AllocationForm.js
+++ src/components/AllocationForm.js
@@ -1,48 +1,63 @@
+import React, { useContext, useState } from 'react';
+import { AppContext } from '../context/AppContext';
+
+const AllocationForm = () => {
+    const { dispatch, remaining } = useContext(AppContext);
+    const [name, setName] = useState('');
+    const [cost, setCost] = useState('');
+    const [action, setAction] = useState('');
+
+    const submitEvent = () => {
+        if (cost > remaining) {
+            alert("The value cannot exceed remaining funds £" + remaining);
+            setCost("");
+            return;
+        }
+
+        const expense = {
+            name: name,
+            cost: parseInt(cost),
+        };
+
+        dispatch({
+            type: action === "Reduce" ? 'RED_EXPENSE' : 'ADD_EXPENSE',
+            payload: expense,
+        });
+    };
+
+    return (
+        <div>
+            <div className='row'>
+                <div className="input-group mb-3" style={{ marginLeft: '2rem' }}>
+                    <div className="input-group-prepend">
+                        <label className="input-group-text">Department</label>
+                    </div>
+                    <select className="custom-select" onChange={(e) => setName(e.target.value)}>
+                        <option defaultValue>Choose...</option>
+                        <option value="Marketing">Marketing</option>
+                        <option value="Sales">Sales</option>
+                        <option value="Finance">Finance</option>
+                        <option value="HR">HR</option>
+                        <option value="IT">IT</option>
+                        <option value="Admin">Admin</option>
+                    </select>
+
+                    <div className="input-group-prepend" style={{ marginLeft: '2rem' }}>
+                        <label className="input-group-text">Allocation</label>
+                    </div>
+                    <select className="custom-select" onChange={(e) => setAction(e.target.value)}>
+                        <option value="Add">Add</option>
+                        <option value="Reduce">Reduce</option>
+                    </select>
+
+                    <input type='number' required value={cost} onChange={(e) => setCost(e.target.value)} style={{ marginLeft: '2rem' }} />
+                    <button className="btn btn-primary" onClick={submitEvent} style={{ marginLeft: '2rem' }}>
+                        Save
+                    </button>
+                </div>
+            </div>
+        </div>
+    );
+};
+
 export default AllocationForm;
-
-
-  {
-                        /* Add Budget component here */
-                    }
-                    <div className='col-sm'>
-                        <Budget />
-                    </div>
-
-                    {
-                        /* Add Remaining component here*/
-                    }
-                    <div className='col-sm'>
-                        <Remaining />
-                    </div>
-                    {
-                        /* Add ExpenseTotal component here */
-                    }
-                    <div className='col-sm'>
-                        <ExpenseTotal />
-                    </div>
-                    {
-                        /* Add ExpenseList component here */
-                    }
-                    <h3 className='mt-3'>Allocation</h3>
-                    <div className='row '>
-                        <div className='col-sm'>
-                            <ExpenseList />
-                        </div>
-                    </div>
-                    {
-                        /* Add ExpenseItem component here */
-                    }
-                    <div className='col-sm'>
-                        <ExpenseItem />
-                    </div>
-                    {
-                        /* Add AllocationForm component here under */
-                    }
-
-                    <h3 className='mt-3'>Change allocation</h3>
-                    <div className='row mt-3'>
-                        <div className='col-sm'>
-                            <AllocationForm />
-                        </div>
-                    </div>
-
