ffe99de6f39d04f9cff2bbd6be71e4486fcc6b2e
Rania-cloud <raniaas032006@outlook.fr>
2025-05-03 00:39:31 +0200

    Update ExpenseTotal.js

--- src/components/ExpenseTotal.js
+++ src/components/ExpenseTotal.js
@@ -1,20 +1,15 @@
-ExpenseTotal.js 
-
 import React, { useContext } from 'react';
 import { AppContext } from '../context/AppContext';
+
 const ExpenseTotal = () => {
     const { expenses } = useContext(AppContext);
-    const totalExpenses = expenses.reduce((total, item) => {
-        return (total += item.cost);
-    }, 0);
+    const totalExpenses = expenses.reduce((total, item) => total + item.cost, 0);
+
     return (
         <div className='alert alert-primary'>
             <span>Spent so far: £{totalExpenses}</span>
         </div>
     );
 };
+
 export default ExpenseTotal;
-
-
-                    
-
