d0949b4368c82bb6c83a99e06b00cece48e43d25
Rania-cloud <raniaas032006@outlook.fr>
2025-05-03 00:39:07 +0200

    Update Remaining.js

--- src/components/Remaining.js
+++ src/components/Remaining.js
@@ -1,24 +1,16 @@
-Remaining.js 
-
 import React, { useContext } from 'react';
 import { AppContext } from '../context/AppContext';
+
 const Remaining = () => {
     const { expenses, budget } = useContext(AppContext);
-    const totalExpenses = expenses.reduce((total, item) => {
-        return (total = total + item.cost);
-    }, 0);
+    const totalExpenses = expenses.reduce((total, item) => total + item.cost, 0);
     const alertType = totalExpenses > budget ? 'alert-danger' : 'alert-success';
+
     return (
         <div className={`alert ${alertType}`}>
             <span>Remaining: £{budget - totalExpenses}</span>
         </div>
     );
 };
+
 export default Remaining;
-
-
-//Remaining component
-<div className='col-sm'>
-    <Remaining />
-</div>
-
